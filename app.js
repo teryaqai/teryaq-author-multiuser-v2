5e443e4988aad26218fabb82f7d34245a4427391
teryaqai <teryaqai@gmail.com>
2026-09-22 03:50:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 (() => {
 'use strict';
 
-const APP_VERSION = '2.1.0';
+const APP_VERSION = '2.1.1';
 const SCHEMA_VERSION = '2.0.0';
 const DB_NAME = 'TeryaqAuthorDB';
 const DB_VERSION = 2;
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='teryaq-author-v2.1.0';
+const CACHE='teryaq-author-v2.1.1';
 const ASSETS=['./','./index.html','./styles.css','./platform.js','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
